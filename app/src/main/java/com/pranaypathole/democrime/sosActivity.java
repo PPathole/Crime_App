206f577c85738e36c57db104feda1a06184ab829
PPathole <ppathole7@gmail.com>
2018-10-04 21:29:02 +0530

    UI overhaul, SOS Morse code added

---
+++ app/src/main/java/com/pranaypathole/democrime/sosActivity.java
@@ -0,0 +1,37 @@
+package com.pranaypathole.democrime;
+
+import android.content.Intent;
+import android.graphics.Camera;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class sosActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sos);
+    }
+
+    public void dialPolice(View view){
+        Intent i = new Intent(Intent.ACTION_DIAL);
+        i.setData(Uri.parse("tel:100"));
+        startActivity(i);
+    }
+
+    public void playmorse(View view){
+        MediaPlayer mplayer = MediaPlayer.create(this, R.raw.morsecode);
+        mplayer.start();
+    }
+
+
+    public void contact(View view){
+        Intent callintent = new Intent(Intent.ACTION_DIAL);
+        startActivity(callintent);
+    }
+}
