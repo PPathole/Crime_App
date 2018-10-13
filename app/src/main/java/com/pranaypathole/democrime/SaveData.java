418a69686e6a21fde9efe99500b7257a7b000320
PPathole <ppathole7@gmail.com>
2018-10-13 22:34:13 +0530

    Complaints being pushed to Firebase DB

---
+++ app/src/main/java/com/pranaypathole/democrime/SaveData.java
@@ -0,0 +1,19 @@
+package com.pranaypathole.democrime;
+
+public class SaveData {
+
+    String complaintBox;
+
+    public String getComplaintBox() {
+        return complaintBox;
+    }
+
+    public void setComplaintBox(String complaintBox) {
+        this.complaintBox = complaintBox;
+    }
+
+    public SaveData(String complaintBox) {
+
+        this.complaintBox = complaintBox;
+    }
+}
--- app/src/main/java/com/pranaypathole/democrime/complaint.java
+++ app/src/main/java/com/pranaypathole/democrime/complaint.java
@@ -1,22 +1,55 @@
 package com.pranaypathole.democrime;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class complaint extends AppCompatActivity {
 
-    public void submitReport(View view){
-        Toast.makeText(this, "Report Submitted!", Toast.LENGTH_SHORT).show();
-        Intent gotoIntent = new Intent(this, homepage.class);
-        startActivity(gotoIntent);
-    }
+    Spinner complaintSpinner;
+    EditText descriptionBox;
+
+    ProgressDialog mDialog;
+
+    DatabaseReference mDatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complaint);
+
+        complaintSpinner = findViewById(R.id.complaintSpinner);
+        descriptionBox = findViewById(R.id.descriptionBox);
+
+        mDialog = new ProgressDialog(this);
+        mDialog.setMessage("Submitting...");
+
+
+
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference().child("Complaints");
+    }
+
+    public void submitReport(View view){
+        AddData();
+        Intent myIntent = new Intent(this, homepage.class);
+        startActivity(myIntent);
+    }
+
+    public void AddData(){
+        String complaintBox = descriptionBox.getText().toString();
+        mDialog.show();
+        SaveData saveData = new SaveData(complaintBox);
+        mDatabaseReference.push().setValue(saveData);
+        mDialog.dismiss();
+        Toast.makeText(this, "Report Filed!", Toast.LENGTH_SHORT).show();
     }
 }
